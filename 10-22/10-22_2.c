6c0ca55e40a973c49b060621e9ba549f00c1a9b8
dfdyz <2649111464@qq.com>
2022-10-22 14:57:50 +0800

    10-22

---
+++ 10-22/10-22_2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	int a=-200; int b=324; char c=254;
+	
+	printf("%d,%d,%d\n",a,b,c);
+	printf("%u,%u,%u\n",a,b,c);
+	printf("%x,%x,%x\n",a,b,c);
+	printf("%c,%c,%c\n",a,b,c);
+	
+	
+	return 0;
+}
+
---
+++ 10-22/10-22_3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+	long al = 1234567890;
+	float af1 = 1234567890;
+	float af2 = 9234567890;
+	double ad = 1234567890;
+	double ad2 = 12345678901234567;
+	
+	printf(" al = %d\n",al);
+	printf(" af1 = %f\n",af1);
+	printf(" af2 = %f \n",af2);
+	printf(" ad = %f\n",ad);
+	printf(" ad2 = %f\n", ad2);
+	
+	
+	return 0;
+}
+
