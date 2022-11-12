d12afbd8df11db1c24400ab66c320a332d1c7fe9
dfdyz <72342988+dfdyz@users.noreply.github.com>
2022-11-12 14:04:09 +0800

    我是傻逼，想复杂了md

--- 11-05/6.c
+++ 11-05/6.c
@@ -5,43 +5,20 @@
 #define orep(i,a,b) for(int i=(a); i<(b); ++i)
 #define crep(i,a,b) for(int i=(a); i<=(b); ++i)
 
-#define DS 10005
-int dp[DS];
-int func[DS][3];
+int func[3];
 
 int main()
 {
-	int n;
+	int n,a,b,c;
 	scanf("%d",&n);
-
-	crep(i,1,n) {
-		int flag = -1, w=0;
-		dp[i] = INT_MAX;
-		if(i-1 >= 0 && dp[i-1] <= dp[i]) {
-			dp[i] = dp[i-1];
-			flag = 0;
-			w=1;
-		}
-		if(i-5 >= 0 && dp[i-5] <= dp[i]) {
-			dp[i] = dp[i-5];
-			flag = 1;
-			w=5;
-		}
-		if(i-10 >= 0 && dp[i-10] <= dp[i]) {
-			dp[i] = dp[i-10];
-			flag = 2;
-			w=10;
-		}
-		dp[i]++;
-		func[i][0] = func[i-w][0];
-		func[i][1] = func[i-w][1];
-		func[i][2] = func[i-w][2];
-		func[i][flag]++;
-	}
 	
-	crep(i,0,func[n][2]){
-		crep(j,0,func[n][1]+2*i){
-			printf("%d张1块, %d张5块, %d张10块\n",func[n][0]+5*j,func[n][1]-j+i*2,func[n][2]-i);
+	c = n/10;
+	b = (n%10)/5;
+	a = n%5;
+	
+	crep(i,0,c){
+		crep(j,0,b+2*i){
+			printf("%d张1元, %d张5元, %d张10元\n",a+5*j,b-j+i*2,c-i);
 		}
 	}
 
